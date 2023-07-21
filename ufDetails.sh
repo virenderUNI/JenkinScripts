157bb64b59cd91d93bab71e7d69b4c92226a9dec
“virenderUNI” <virender.singh@unicommerce.com>
2023-07-21 18:22:00 +0530

    Correcting Recipient email

--- ufDetails.sh
+++ ufDetails.sh
@@ -21,7 +21,8 @@ echo "Report file: ${reportFilename}"
 
 # MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com,dixit.garg@unicommerce.com,ankur.pratik@unicommerce.com,ankit.jain03@unicommerce.com,bhupi@unicommerce.com,kapil@unicommerce.com,prateek.mahajan@unicommerce.com,adarsh.bajpai@unicommerce.com,rakshit.jain@unicommerce.com,oncall@unicommerce.com"
 
-MAIL_RECIPIENTS="virender.singh@unicommerce.com," + $4
+MAIL_RECIPIENTS="virender.singh@unicommerce.com,"
+MAIL_RECIPIENTS+=$4
 
 echo ${MAIL_RECIPIENTS}
 
