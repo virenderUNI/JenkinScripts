bea4b4dbab2f7239b363664673e5a9c2c28886bb
“virenderUNI” <virender.singh@unicommerce.com>
2023-07-21 17:34:23 +0530

    Change for date in shell

--- ufDetails.sh
+++ ufDetails.sh
@@ -8,7 +8,7 @@ ls -1 /tmp/uf-summary-*
 reportFilename=`ls -1t /tmp/uf-details-* | head -1`
 echo "Report file: ${reportFilename}"
 
-yesterday_date=$(date -d "yesterday 13:00" +'%d-%b-%Y')
+yesterday_date=$(date -d "$DetailsDated" +'%d-%b-%Y')
 
 # MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com,dixit.garg@unicommerce.com,ankur.pratik@unicommerce.com,ankit.jain03@unicommerce.com,bhupi@unicommerce.com,kapil@unicommerce.com,prateek.mahajan@unicommerce.com,adarsh.bajpai@unicommerce.com,rakshit.jain@unicommerce.com,oncall@unicommerce.com"
 
