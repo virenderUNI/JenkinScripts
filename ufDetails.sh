45226efa6d992771a9d6851120123643855c80e7
“virenderUNI” <virender.singh@unicommerce.com>
2023-07-21 18:04:33 +0530

    Remaining Emails Added

--- ufDetails.sh
+++ ufDetails.sh
@@ -21,7 +21,7 @@ echo "Report file: ${reportFilename}"
 
 # MAIL_RECIPIENTS="sourabh.shrivastava@unicommerce.com,dixit.garg@unicommerce.com,ankur.pratik@unicommerce.com,ankit.jain03@unicommerce.com,bhupi@unicommerce.com,kapil@unicommerce.com,prateek.mahajan@unicommerce.com,adarsh.bajpai@unicommerce.com,rakshit.jain@unicommerce.com,oncall@unicommerce.com"
 
-MAIL_RECIPIENTS="virender.singh@unicommerce.com"
+MAIL_RECIPIENTS="virender.singh@unicommerce.com,ankur.pratik@unicommerce.com,sourabh.shrivastava@unicommerce.com"
 
 MAIL_SUBJECT="Unfulfillable Sale Order Details | ${yesterday_date}"
 MAIL_CONTENT="Please find the attachment. Report prepared by alpha team"
